2ca80702718daad782be05b1937766821889910b
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-12 16:05:42 +0700

    Awal

---
+++ KodeBuah.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302204126
+{
+    internal class KodeBuah
+    {
+        
+       public enum buah
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+        }
+
+        public static string getKodeBuah(buah buah)
+        {
+            string[] kodeBuah = {"A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "M00", "N00", "O00"};
+
+            return kodeBuah[(int)buah];
+        }
+    }
+}
