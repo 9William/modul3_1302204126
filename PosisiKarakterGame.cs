a71ed64fc90c78f318505d7eb390eaaab6a04b7a
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-03-12 17:23:07 +0700

    menambahkan class PosisiKarakterGame

---
+++ PosisiKarakterGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302204126
+{
+    class PosisiKarakterGame
+    {
+        public enum State
+        {
+            Jongkok, Berdiri, Tengkurap, Terbang
+        }
+
+        public enum Trigger
+        {
+            TombolW, TombolS, TombolX
+        }
+
+        
+
+        public State prevState;
+        public State nextState;
+        public State currentState;
+        public Trigger trigger;
+
+        public void transition(State prevState, State nextState, Trigger trigger)
+        {
+            this.prevState = prevState;
+            this.nextState = nextState;
+            this.trigger = trigger;
+
+            
+        }
+
+        public State getNextState(State prevState, Trigger trigger)
+        {
+            State nextState = prevState;
+
+            return nextState;
+        }
+
+        
+
+        public void activateTrigger(Trigger trigger)
+        {
+            State nextStage = getNextState(currentState, trigger);
+            currentState = nextStage;
+
+            transition(nextStage, nextState, trigger);
+        }
+
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -6,23 +6,23 @@ namespace modul3_1302204126
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Apel));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Aprikot));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Alpukat));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Pisang));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Paprika));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Blackberry));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Ceri));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kelapa));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Jagung));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kurma));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Durian));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Anggur));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Melon));
-            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Semangka));
-            Console.WriteLine("");
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Apel));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Aprikot));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Alpukat));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Pisang));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Paprika));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Blackberry));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Ceri));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kelapa));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Jagung));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kurma));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Durian));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Anggur));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Melon));
+            //Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Semangka));
+            //Console.WriteLine("");
 
-
+            PosisiKarakterGame.play();
 
         }
     }
